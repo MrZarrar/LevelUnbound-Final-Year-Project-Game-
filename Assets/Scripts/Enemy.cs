f285611e67d0306519ba234c860de56adf0bf1b8
MrZarrar <m.mushafzarrar@gmail.com>
2025-10-14 17:02:20 +0100

    added Navmesh agent system to Enemy with an aggro and attack range and it follows player #3

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,20 +7,65 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] float health = 3;
 
+    [Header("Combat")]
+    [SerializeField] float attackCD = 2f;
+    [SerializeField] float attackRange = 1.5f;
+    [SerializeField] float aggroRange = 4f;
+
+    GameObject player;
+    NavMeshAgent agent; 
     Animator animator;
+    float timePassed;
+    float newDestinationCD = 0.5f;
 
     void Start()
     {
+        agent = GetComponent<NavMeshAgent>(); 
         animator = GetComponent<Animator>();
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
-	
+
+    void Update()
+    {
+        animator.SetFloat("speed", agent.velocity.magnitude / agent.speed);
+
+ 		if (player == null)
+		{
+            return;
+		} 
+
+        if (timePassed >= attackCD)
+        {
+            if (Vector3.Distance(player.transform.position, transform.position) <= attackRange)
+            {
+                animator.SetTrigger("attack");
+                timePassed = 0;
+            }
+        }
+        timePassed += Time.deltaTime;
+
+        if (newDestinationCD <= 0 && Vector3.Distance(player.transform.position, transform.position) <= aggroRange)
+        {
+            newDestinationCD = 0.5f;
+            agent.SetDestination(player.transform.position);
+        }
+        newDestinationCD -= Time.deltaTime;
+        transform.LookAt(player.transform);
+    } 
+
+	private void OnCollisionEnter(Collision collision)
+	{
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            print(true);
+            player = collision.gameObject;
+        }
+    }
 
     void Die()
     {
-      
         Destroy(gameObject);
-}
+    }
 
     public void TakeDamage(float damageAmount)
     {
@@ -33,4 +78,13 @@ public class Enemy : MonoBehaviour
         }
     }
 
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, aggroRange);
+    } 
 } 
