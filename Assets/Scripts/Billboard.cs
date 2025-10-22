05a11808761449163359dc5d31f61353b9acea8b
MrZarrar <m.mushafzarrar@gmail.com>
2025-10-22 12:06:21 +0100

    updated healthbar to change colour (G-Y-R) and added a billboard class to make the healthbar follow camera #4

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+
+    public Transform cam;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.LookAt(transform.position + cam.forward);
+    }
+}
--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -1,19 +1,26 @@
 using System.Collections;
-using system.Collections.Generic;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
     public Slider slider;
+    public Gradient gradient;
+    public Image fill;
 
     public void SetMaxHP(int hp)
     {
         slider.maxValue = hp;
         slider.value = hp;
+
+        fill.color = gradient.Evaluate(1f);
     }
     public void SetHP(int hp)
     {
         slider.value = hp;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+
     }
 }
