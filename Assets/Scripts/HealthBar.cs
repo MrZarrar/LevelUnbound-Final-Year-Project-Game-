25149e5d99a344c2eeb467b299f748b9ed82b121
MrZarrar <m.mushafzarrar@gmail.com>
2025-11-04 12:48:25 +0000

    added mana bar #4

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class HealthBar : MonoBehaviour
 {
@@ -9,18 +10,38 @@ public class HealthBar : MonoBehaviour
     public Gradient gradient;
     public Image fill;
 
+    [Header("Text Display")] 
+    [SerializeField] private TextMeshProUGUI hpText; 
+    private int currentMaxHealth; 
+
+
+
     public void SetMaxHP(int hp)
     {
         slider.maxValue = hp;
         slider.value = hp;
 
         fill.color = gradient.Evaluate(1f);
+
+        currentMaxHealth = hp;
+        if (hpText != null) 
+        {
+            
+            hpText.text = $"{hp} / {hp}";
+        }
+
     }
     public void SetHP(int hp)
     {
         slider.value = hp;
 
         fill.color = gradient.Evaluate(slider.normalizedValue);
+
+        if (hpText != null) 
+        {
+
+            hpText.text = $"{hp} / {currentMaxHealth}";
+        }
 
     }
 }
