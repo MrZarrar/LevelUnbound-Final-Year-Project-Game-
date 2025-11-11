4f995505a034d85cf2eb26339e7d069a5930eb5a
MrZarrar <m.mushafzarrar@gmail.com>
2025-11-11 15:17:20 +0000

    Added a scriptable file to add different enemy types #10

---
+++ Assets/Scripts/EnemyData.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyData", menuName = "Enemy Data")]
+public class EnemyData : ScriptableObject
+{
+    public GameObject enemyPrefab;
+    
+    [Header("Enemy Stats")]
+    public float health = 3;
+    public int XPDrop = 10;
+
+    public float agentSpeed = 3.5f;
+
+    public float attackCD = 2f;
+
+    public float attackRange = 1.5f;
+    public float aggroRange = 4f;
+
+    public float weaponDamage = 1f;
+
+    public bool isRanged = false;
+
+    public GameObject projectilePrefab;
+}
+
+
+
+
+
+
